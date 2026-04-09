eba3de21c4570ba07abb554ac26067a4b2cd816d
DevJuan001 <juanesyt7@gmail.com>
2026-04-08 19:48:35 -0500

    refactor: rename product_brand to product_brand_name for clarity in product model

--- backend/app/models/product_model.py
+++ backend/app/models/product_model.py
@@ -5,6 +5,6 @@ class Product(BaseModel):
     input_order_id: int
     subcategory_id: int
     product_serial: str
-    product_brand: int
+    product_brand_name: int
     product_details_id: int
     product_garanty_input: datetime
