201b1c8bf62d6e40650ed264d3973c06e5ef5a20
DevJuan001 <juanesyt7@gmail.com>
2026-04-09 14:09:52 -0500

    feat: include credentials in deleteSubcategory API request for improved session handling

--- frontend_web/react+vite/src/modules/subcategories/services/deleteSubcategoryService.js
+++ frontend_web/react+vite/src/modules/subcategories/services/deleteSubcategoryService.js
@@ -4,6 +4,7 @@ import { getToken } from "../../../utils/auth";
 export async function deleteSubcategoryService(subcategory_id) {
     const res = await fetch(`${apiRoutes.apiUrl}${apiRoutes.subcategories}/delete/${subcategory_id}`, {
         method: "DELETE",
+        credentials: "include",
         headers: {
             Authorization: getToken()
         }
