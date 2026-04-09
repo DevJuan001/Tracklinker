00c0d01adbe973192df8b238c2a9704041a8ba9c
DevJuan001 <juanesyt7@gmail.com>
2026-04-08 23:05:13 -0500

    feat: add UpdateProduct model to support product updates with additional fields

--- backend/app/models/product_model.py
+++ backend/app/models/product_model.py
@@ -8,3 +8,12 @@ class Product(BaseModel):
     product_brand_name: int
     product_details_id: int
     product_garanty_input: datetime
+
+class UpdateProduct(BaseModel):
+        product_id: int
+        input_order_id: int
+        subcategory_id: int
+        product_serial: str
+        product_brand_id: int
+        product_details_id: int
+        product_garanty_input: datetime
