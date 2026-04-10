fe384c49e0b5011bbc605d8279ce844b2c92f6ea
DevJuan001 <juanesyt7@gmail.com>
2026-04-10 14:13:05 -0500

    fix: add missing key prop to SubcategoriesItem in SubcategoriesList

--- frontend_web/react+vite/src/modules/subcategories/components/ui/SubcategoriesList.jsx
+++ frontend_web/react+vite/src/modules/subcategories/components/ui/SubcategoriesList.jsx
@@ -20,6 +20,7 @@ export default function SubcategoriesList({
         {subcategories.map((subcategory) => (
           // Subcategorías
           <SubcategoriesItem
+            key={subcategory.subcategory_id}
             subcategory={subcategory}
             moreInfoOnClick={(e) => {
               e.stopPropagation();
