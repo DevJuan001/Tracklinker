62033511c2d4665d289a6bab0831813610297f60
DevJuan001 <juanesyt7@gmail.com>
2026-04-11 15:53:42 -0500

    feat: create actionsIcons module to centralize action icon imports and remove mainIcons for better organization

---
+++ frontend_web/react+vite/src/assets/icons/actionsIcons.js
@@ -0,0 +1,25 @@
+import moreInfoIcon from "./main/actions/more-info-icon.svg";
+import editInfoIcon from "./main/actions/edit-info-icon.svg";
+import deleteIcon from "./main/actions/delete-user-icon.svg";
+import filterIcon from "./main/actions/filter-icon.svg";
+import uploadIcon from "./main/actions/upload-icon.svg";
+import addIcon from "./main/actions/add-icon.svg";
+import visibility from "./main/actions/visibility.svg";
+import lockVisibility from "./main/actions/visibility-lock.svg";
+import arrowBack from "./main/actions/arrow-back.svg";
+import exportIcon from "./main/actions/export-icon.svg";
+import sendIcon from "./main/actions/send-icon.svg";
+
+export const actionsIcons = {
+  moreInfoIcon,
+  editInfoIcon,
+  deleteIcon,
+  filterIcon,
+  addIcon,
+  uploadIcon,
+  visibility,
+  lockVisibility,
+  arrowBack,
+  exportIcon,
+  sendIcon,
+};
