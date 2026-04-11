f2531d1cbfb8d742136915c3ad15b292ba926dc4
DevJuan001 <juanesyt7@gmail.com>
2026-04-11 00:22:56 -0500

    fix: add auto-close functionality to DownloadToast after 5 seconds

--- frontend_web/react+vite/src/modules/dashboard/components/modals/DownloadToast.jsx
+++ frontend_web/react+vite/src/modules/dashboard/components/modals/DownloadToast.jsx
@@ -1,17 +1,23 @@
 import { modalIcons } from "../../../../assets/icons/modalIcons";
 
-export default function DownloadToast({ onClose }) {
+export default function DownloadToast({ showDownloadToast, onClose }) {
+  if (showDownloadToast === true) {
+    setTimeout(() => {
+      onClose();
+    }, 5000);
+  }
+
   return (
     <section
       className="fixed inset-0 z-10 flex items-end justify-end gap-5 pr-5 pb-5 animate-blurUp
     dark:text-white"
     >
       <div
-        className="relative flex gap-2 border bg-green-100 rounded-lg shadow-lg p-4 cursor-pointer animate-blur
-      dark:bg-black"
+        className="relative flex gap-2 border bg-white rounded-xl shadow-lg p-4 animate-blur
+      dark:bg-black dark:border-[#7e77773b]"
       >
         <div className="flex items-center gap-3">
-          <div className="w-10 h-10 flex items-center justify-center bg-white rounded-full">
+          <div className="w-10 h-10 flex items-center justify-center bg-green-700 rounded-full">
             <img src={modalIcons.confirmIcon} alt="" />
           </div>
           <div className="flex flex-col">
