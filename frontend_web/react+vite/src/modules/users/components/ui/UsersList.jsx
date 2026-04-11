eb1c3cca2efff8c073861fda5e7096ded9c7631b
DevJuan001 <juanesyt7@gmail.com>
2026-04-11 00:01:59 -0500

    feat: add no users message to UsersList component

--- frontend_web/react+vite/src/modules/users/components/ui/UsersList.jsx
+++ frontend_web/react+vite/src/modules/users/components/ui/UsersList.jsx
@@ -2,17 +2,19 @@ import UserItem from "./UserItem";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-export default function UsersList({
-  users,
-  loading,
-  refetch,
-  openModal,
-}) {
+export default function UsersList({ users, loading, refetch, openModal }) {
+  const noUsers = users.length === 0 && !loading;
   const isFirstLoad = users.length === 0 && loading;
+
   return (
     /* Contenedor de los usuarios */
     <section className="max-h-[95%] max-w-full overflow-x-auto overflow-y-auto overflow-hidden">
       <ul className="flex flex-col gap-1">
+        {noUsers && (
+          <span className="text-center dark:text-white pt-5">
+            No se encontraron usuarios
+          </span>
+        )}
         {isFirstLoad ? (
           <SkeletonTheme baseColor="#f3eef5" highlightColor="#c5c1c7">
             <li>
