5f8b3f8fb1a9dfe327ca4c63ba9c26590795c1ac
DevJuan001 <juanesyt7@gmail.com>
2026-04-11 21:58:19 -0500

    refactor: replace getToken with fetchWithAuth in users services for improved authentication handling

--- frontend_web/react+vite/src/modules/reports/services/users/getUsersPieDataService.js
+++ frontend_web/react+vite/src/modules/reports/services/users/getUsersPieDataService.js
@@ -1,15 +1,11 @@
 import { apiRoutes } from "../../../../config/apiRoutes";
-import { getToken } from "../../../../utils/auth";
+import { fetchWithAuth } from "../../../../utils/fetchWithAuth";
 
 export async function getUsersPieDataService(period, signal) {
-  const response = await fetch(
+  const response = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.reports}/get_users_by_rol/${period}`,
     {
       method: "GET",
-      credentials: "include",
-      headers: {
-        Authorization: getToken(),
-      },
       signal,
     },
   );
