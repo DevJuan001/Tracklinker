79207b8a155b782ec1f851eb79d5b0cb9f2d2f28
DevJuan001 <juanesyt7@gmail.com>
2026-04-12 19:54:43 -0500

    style: adjust EditUserInfoModal layout for improved spacing and responsiveness

--- frontend_web/react+vite/src/modules/users/components/modals/EditUserInfoModal.jsx
+++ frontend_web/react+vite/src/modules/users/components/modals/EditUserInfoModal.jsx
@@ -28,7 +28,7 @@ export default function EditUserInfoModal({ user, onClose }) {
 
   return (
     <section className="flex flex-col items-center">
-      <form action="" className="flex flex-col gap-2">
+      <form action="" className="w-full flex flex-col gap-2 px-20">
         <SelectMenu
           name={"rol_id"}
           value={form.rol_id}
