7bfe03cc52a41598c6719b3a337926c77d1b8f6e
DevJuan001 <juanesyt7@gmail.com>
2026-04-13 15:04:47 -0500

    feat: enhance CategoriesList component with loading skeleton and no categories message

--- frontend_web/react+vite/src/modules/categories/components/ui/CategoriesList.jsx
+++ frontend_web/react+vite/src/modules/categories/components/ui/CategoriesList.jsx
@@ -1,43 +1,49 @@
-import Loader from "../../../../globals/components/ui/Loader";
 import CategoryItem from "./CategoryItem";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function CategoriesList({
   categories,
   loading,
-  error,
   openModal,
   refetch,
 }) {
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (loading) {
-    return <Loader />;
-  }
+  const noCategories = categories.length === 0 && !loading;
+  const isFirstLoad = categories.length === 0 && loading;
 
   return (
     /* Contenedor de categorías */
     <section className="max-h-[95%] max-w-full overflow-x-auto overflow-y-auto overflow-hidden">
       <ul className="flex flex-col gap-1">
-        {categories.map((category) => (
-          <CategoryItem
-            key={category.category_id}
-            category={category}
-            moreInfoOnClick={(e) => {
-              e.stopPropagation();
-              openModal(category, "info", refetch);
-            }}
-            editButtonOnClick={(e) => {
-              e.stopPropagation();
-              openModal(category, "edit", refetch);
-            }}
-            deleteButtonOnClick={(e) => {
-              e.stopPropagation();
-              openModal(category, "delete", refetch);
-            }}
-          />
-        ))}
+        {noCategories && (
+          <span className="text-center dark:text-white pt-5">
+            No se encontraron categorias
+          </span>
+        )}
+        {isFirstLoad ? (
+          <SkeletonTheme baseColor="#f3eef5" highlightColor="#c5c1c7">
+            <li>
+              <Skeleton height={"68px"} count={13} borderRadius={"8px"} />
+            </li>
+          </SkeletonTheme>
+        ) : (
+          categories.map((category) => (
+            <CategoryItem
+              key={category.category_id}
+              category={category}
+              openModal={openModal}
+              refetch={refetch}
+              moreInfoOnClick={(e) => {
+                e.stopPropagation();
+                openModal(category, "info", refetch);
+              }}
+              editButtonOnClick={(e) => {
+                e.stopPropagation();
+                openModal(category, "edit", refetch);
+              }}
+            />
+          ))
+        )}
       </ul>
     </section>
   );
