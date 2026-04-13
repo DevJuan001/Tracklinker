d4af7bf126c5b9d3ce8f3e44cdc2f3d4188f2285
DevJuan001 <juanesyt7@gmail.com>
2026-04-12 23:04:39 -0500

    style: refactor SubcategoriesActions component for improved readability and add hover effect to more info button

--- frontend_web/react+vite/src/modules/subcategories/components/ui/SubcategoriesActions.jsx
+++ frontend_web/react+vite/src/modules/subcategories/components/ui/SubcategoriesActions.jsx
@@ -1,7 +1,11 @@
 import { actionsIcons } from "../../../../assets/icons/actionsIcons";
 import ActionButtons from "../../../../globals/components/ui/ActionButtons";
 
-export default function SubcategoriesActions({editButtonOnClick, deleteButtonOnClick, moreInfoOnClick}) {
+export default function SubcategoriesActions({
+  editButtonOnClick,
+  deleteButtonOnClick,
+  moreInfoOnClick,
+}) {
   return (
     /* Botones para interactuar con el usuario */
     <nav className="flex gap-4">
@@ -11,6 +15,7 @@ export default function SubcategoriesActions({editButtonOnClick, deleteButtonOnC
       >
         {/* Botón de más información del usuario */}
         <button
+          className="z-10 transition-all duration-500 dark:brightness-200 hover:scale-125"
           onClick={moreInfoOnClick}
         >
           <img src={actionsIcons.moreInfoIcon} alt="" />
