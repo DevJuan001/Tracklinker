bc43cc01b3a0ef6836979e855a2955fa4000e1ae
DevJuan001 <juanesyt7@gmail.com>
2026-04-13 16:15:59 -0500

    feat: update category status styles for improved dark mode support

--- frontend_web/react+vite/src/modules/categories/constants/categoryStatusConfig.js
+++ frontend_web/react+vite/src/modules/categories/constants/categoryStatusConfig.js
@@ -6,7 +6,8 @@ export const categoryStatusConfig = {
     modalType: "enable",
     visibilityIcon: false,
     icon: usersIcons.inactiveCircle,
-    styles: "w-32 bg-gray-50 text-gray-400 border-gray-300 dark:bg-[#1f2937]",
+    styles:
+      "w-32 bg-gray-50 text-gray-400 border-gray-300 dark:border-gray-700 dark:bg-[#75777e80]",
   },
   2: {
     text: "Activa",
@@ -14,6 +15,6 @@ export const categoryStatusConfig = {
     visibilityIcon: true,
     icon: usersIcons.activeCircle,
     styles:
-      "w-20 bg-green-200 text-green-600 border-green-300 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
+      "w-20 bg-green-200 text-green-600 border-green-300 dark:border-green-900 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
   },
 };
