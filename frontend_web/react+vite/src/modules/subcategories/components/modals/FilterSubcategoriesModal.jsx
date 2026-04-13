ae8860939e6549c1a3aac19acd487245826cb7b0
DevJuan001 <juanesyt7@gmail.com>
2026-04-12 23:04:07 -0500

    feat: implement FilterSubcategoriesModal with category selection and date filters

--- frontend_web/react+vite/src/modules/subcategories/components/modals/FilterSubcategoriesModal.jsx
+++ frontend_web/react+vite/src/modules/subcategories/components/modals/FilterSubcategoriesModal.jsx
@@ -1,5 +1,40 @@
 import FilterModal from "../../../../globals/components/modals/FilterModal";
+import SelectMenu from "../../../../globals/components/modals/SelectMenu";
+import { useCategories } from "../../hooks/useCategories";
+import useFilterSubcategories from "../../hooks/useFilterSubcategories";
 
-export default function FilterSubcategoriesModal() {
-  return <FilterModal></FilterModal>;
+export default function FilterSubcategoriesModal({ refetch, onClose }) {
+  const { categories } = useCategories();
+  const { form, handleChange, handleApply } = useFilterSubcategories(
+    {
+      start_date: "",
+      end_date: "",
+      category_order: "",
+    },
+    refetch,
+  );
+  return (
+    <FilterModal
+      applyButtonOnClick={() => {
+        onClose();
+        handleApply();
+      }}
+      orderByStartDateValue={form.start_date}
+      orderByStartDateOnChange={handleChange}
+      orderByFinishDateValue={form.end_date}
+      orderByFinishDateOnChange={handleChange}
+      onClose={onClose}
+    >
+      <SelectMenu
+        spanText={"Categoría"}
+        name={"category_order"}
+        value={form.category_order}
+        onChange={handleChange}
+        options={categories.map((category) => ({
+          value: category.category_id,
+          label: category.category_name,
+        }))}
+      />
+    </FilterModal>
+  );
 }
