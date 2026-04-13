7479a2d1562ab148fd599c6a76008acc48ae8398
DevJuan001 <juanesyt7@gmail.com>
2026-04-13 13:50:37 -0500

    fix: enhance SuppliersList component with loading skeleton and no suppliers message for better user feedback

--- frontend_web/react+vite/src/modules/suppliers/components/ui/SuppliersList.jsx
+++ frontend_web/react+vite/src/modules/suppliers/components/ui/SuppliersList.jsx
@@ -1,31 +1,50 @@
 import SupplierItem from "./SupplierItem";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function SuppliersList({
   suppliers,
   loading,
-  error,
   refetch,
   openModal,
 }) {
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
+  const noSuppliers = suppliers.length === 0 && !loading;
+  const isFirstLoad = suppliers.length === 0 && loading;
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
   return (
     <section className="max-h-[95%] max-w-full flex flex-col gap-1 overflow-x-auto overflow-y-auto">
       <ul className="flex flex-col gap-1">
-        {suppliers.map((supplier) => (
-          <SupplierItem
-            key={supplier.supplier_id}
-            supplier={supplier}
-            moreInfoOnClick={() => openModal(supplier, "info", refetch)}
-            editButtonOnClick={() => openModal(supplier, "edit", refetch)}
-            deleteButtonOnClick={() => openModal(supplier, "delete", refetch)}
-          />
-        ))}
+        {noSuppliers && (
+          <span className="text-center dark:text-white pt-5">
+            No se encontraron proveedores
+          </span>
+        )}
+        {isFirstLoad ? (
+          <SkeletonTheme baseColor="#f3eef5" highlightColor="#c5c1c7">
+            <li>
+              <Skeleton height={"68px"} count={13} borderRadius={"8px"} />
+            </li>
+          </SkeletonTheme>
+        ) : (
+          suppliers.map((supplier) => (
+            <SupplierItem
+              key={supplier.supplier_id}
+              supplier={supplier}
+              moreInfoOnClick={(e) => {
+                e.stopPropagation();
+                openModal(supplier, "info", refetch);
+              }}
+              editButtonOnClick={(e) => {
+                e.stopPropagation();
+                openModal(supplier, "edit", refetch);
+              }}
+              deleteButtonOnClick={(e) => {
+                e.stopPropagation();
+                openModal(supplier, "delete", refetch);
+              }}
+            />
+          ))
+        )}
       </ul>
     </section>
   );
