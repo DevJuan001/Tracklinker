a3af7470329cc48cce48e6225b52e6e933fd9196
DevJuan001 <juanesyt7@gmail.com>
2026-04-13 15:32:42 -0500

    feat: add endpoints for enabling and disabling subcategories, and retrieve active categories

--- backend/app/routes/subcategories_routes.py
+++ backend/app/routes/subcategories_routes.py
@@ -9,24 +9,33 @@ router = APIRouter(
     tags=["subcategories"]
 )
 # Endpoint para obtener todas las subcategorías
+
+
 @router.get("/")
 def get_all_subcategories(
     start_date: str = None,
     end_date: str = None,
     category_order: int = None,
+    status: int = None,
+    name_order: str = None,
 ):
     return SubcategoriesController.get_all_subcategories(
         start_date,
         end_date,
         category_order,
+        status,
+        name_order
     )
 
+# Endpoint para obtener las categorias activas
+@router.get("/categories")
+def get_categories():
+    return SubcategoriesController.get_categories()
 
 # Endpoint para obtener una subcategoría mediante el id
 @router.get("/{subcategory_id}")
 def get_subcategory_by_id(subcategory_id: int):
-    return SubcategoriesController.get_subcategory_by_id(subcategory_id) 
-
+    return SubcategoriesController.get_subcategory_by_id(subcategory_id)
 
 # Endpoint para crear o registrar una subcategoría
 @router.post("/create")
@@ -47,15 +56,18 @@ def update_subcategory(
     return SubcategoriesController.update_subcategory(subcategory_id, subcategory_data)
 
 
-# Endpoint para eliminar una subcategoría mediante su id
-@router.delete("/delete/{subcategory_id}")
-def delete_subcategory(
+# Endpoint para deshabilitiar una subcategoría mediante su id
+@router.put("/disable/{subcategory_id}")
+def disable_subcategory(
     subcategory_id: int,
     payload: dict = Depends(require_roles(["Admin"]))
 ):
-    return SubcategoriesController.delete_subcategory(subcategory_id) 
+    return SubcategoriesController.disable_subcategory(subcategory_id)
 
-     
-
-
-
+# Endpoint para habilitar una subcategoría mediante su id
+@router.put("/enable/{subcategory_id}")
+def enable_subcategory(
+    subcategory_id: int,
+    payload: dict = Depends(require_roles(["Admin"]))
+):
+    return SubcategoriesController.enable_subcategory(subcategory_id)
