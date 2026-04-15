8b7524efe65609f105fb06268aeee1c6ea2f85f3
DevJuan001 <juanesyt7@gmail.com>
2026-04-15 18:27:56 -0500

    feat: enhance SelectMenu component with improved styling and accessibility features

--- frontend_web/react+vite/src/globals/components/modals/SelectMenu.jsx
+++ frontend_web/react+vite/src/globals/components/modals/SelectMenu.jsx
@@ -24,26 +24,33 @@ export default function SelectMenu({
 
   return (
     <section className="relative w-full flex flex-col gap-1">
-      <span className="text-sm dark:text-white">{spanText}</span>
       <div className="w-full flex items-center gap-1.5">
         <div
+          tabIndex={0}
           onClick={() => setOpen(!open)}
-          className="w-full h-14 pr-2 flex items-center border border-[#a1a1a131] 
-            bg-[#e5e5e527] rounded-2xl cursor-pointer text-sm
-            dark:bg-[#ffffff1a] dark:border-[#ffffff15] dark:text-white"
+          className="relative w-full h-16 pr-2 pt-2 flex items-center border border-[#a1a1a131] 
+          rounded-2xl cursor-pointer text-sm transition-all duration-300 focus:shadow-[0_0_2px_1px_#e5e7eb]
+          dark:border-[#28282b] dark:text-[#E4E2E5] dark:focus:focus:shadow-[0_0_4px_2px_#ffffff33]"
         >
-          <div className="w-full h-11 flex items-center pl-5">
-            {options.find((opt) => {
-              if (opt.value !== "" && !isNaN(opt.value)) {
-                return Number(opt.value) === Number(value);
-              }
-              return String(opt.value) === String(value);
-            })?.label ?? "Seleccionar"}
+          <div className="absolute top-2 left-3 px-1">
+            <span className="text-xs bg-white text-[#7E777E] dark:bg-black dark:text-[#b4aab4]">
+              {spanText}
+            </span>
+          </div>
+          <div className="w-full h-full flex pl-4 pt-3">
+            <div className="w-full flex items-center text-base">
+              {options.find((opt) => {
+                if (opt.value !== "" && !isNaN(opt.value)) {
+                  return Number(opt.value) === Number(value);
+                }
+                return String(opt.value) === String(value);
+              })?.label ?? "Seleccionar"}
+            </div>
           </div>
           <img
             src={modalIcons.arrowUp}
             alt=""
-            className={`transition-transform duration-200 ${open ? "rotate-180" : "rotate-0"} dark:invert`}
+            className={`-translate-y-1 transition-transform duration-200 ${open ? "rotate-180" : "rotate-0"} dark:invert`}
           />
         </div>
         <button
@@ -53,9 +60,9 @@ export default function SelectMenu({
           }}
           disabled={addButtonInvisible}
           type="button"
-          className={`w-16 h-14 flex items-center justify-center border rounded-2xl bg-[#e5e5e527]
+          className={`w-16 h-16 flex items-center justify-center border rounded-2xl bg-[#e5e5e527]
           ${addButtonInvisible ? "hidden" : "opacity-100"} 
-          dark:bg-[#ffffff1a] dark:border-[#ffffff15]`}
+          dark:bg-black dark:border-[#28282b]`}
         >
           <img src={addIcon} className="h-4 w-4 dark:invert" />
         </button>
@@ -65,7 +72,7 @@ export default function SelectMenu({
         <div
           onClick={() => setOpen(false)}
           className="absolute top-full left-0 w-full max-h-96 overflow-y-auto rounded-2xl border bg-white shadow-lg z-[400]
-        dark:bg-[#1a1a1a] dark:text-white dark:border-none"
+        dark:bg-black dark:text-white dark:border-none"
         >
           {options.map((option) => (
             <div
