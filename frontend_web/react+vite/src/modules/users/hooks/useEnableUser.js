199d1aefa19fa6c642a957802865fcdba949d4a5
DevJuan001 <juanesyt7@gmail.com>
2026-04-17 00:20:01 -0500

    refactor: update handleSubmit function in useEnableUser hook to close modal on success and remove innerModal handling

--- frontend_web/react+vite/src/modules/users/hooks/useEnableUser.js
+++ frontend_web/react+vite/src/modules/users/hooks/useEnableUser.js
@@ -5,16 +5,15 @@ export function useEnableUser(userId) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function handleSubmit(e, setInnerModal) {
+  async function handleSubmit(e, closeModal) {
     e.preventDefault();
     setLoading(true);
     try {
       const response = await enableUserService(userId);
       if (response.success) {
-        setInnerModal("success");
+        closeModal();
       }
     } catch (error) {
-      setInnerModal("error");
       setError(error);
     } finally {
       setLoading(false);
