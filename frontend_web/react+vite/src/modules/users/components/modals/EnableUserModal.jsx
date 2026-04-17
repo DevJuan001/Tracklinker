4238ffdf2174ec70805bd2e939ee85a650a562e2
DevJuan001 <juanesyt7@gmail.com>
2026-04-17 00:21:36 -0500

    refactor: simplify EnableUserModal by removing inner modal handling and updating confirm button logic to close modal on success

--- frontend_web/react+vite/src/modules/users/components/modals/EnableUserModal.jsx
+++ frontend_web/react+vite/src/modules/users/components/modals/EnableUserModal.jsx
@@ -1,16 +1,10 @@
+// Hooks
+import { useEnableUser } from "../../hooks/useEnableUser";
 // Componentes
 import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
-// Modales
-import SuccessModal from "../../../../globals/components/modals/SuccessModal";
-import ErrorModal from "../../../../globals/components/modals/ErrorModal";
-// Hooks
-import { useEnableUser } from "../../hooks/useEnableUser.js";
-import { useState } from "react";
-
 export default function EnableUserModal({ user, onClose }) {
-  const [innerModal, setInnerModal] = useState(null);
-  const { handleSubmit, loading} = useEnableUser(user.id);
+  const { handleSubmit, loading } = useEnableUser(user.id);
   return (
     <section className="flex flex-col justify-center items-center dark:text-white">
       <p>
@@ -26,30 +20,9 @@ export default function EnableUserModal({ user, onClose }) {
         confirmText={loading ? <Loader /> : "Habilitar"}
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={(e) => handleSubmit(e, setInnerModal)}
+        confirmButtonOnClick={(e) => handleSubmit(e, onClose)}
         cancelButtonOnClick={onClose}
       />
-      {innerModal === "success" && (
-        <SuccessModal
-        isOpen={true}
-        confirmButtonText={"Volver a la página"}
-        confirmTitle={"¡Usuario habilitado correctamente!"}
-        confirmText={"El usuario ha sido habilitado correctamente."}
-        onClose={() => {
-            setInnerModal(null);
-            onClose();
-        }}
-        />
-      )}
-      {innerModal === "error" && (
-        <ErrorModal
-        isOpen={true}
-        confirmButtonText={"Volver a intentarlo"}
-        errorTitle={"¡No se pudo habilitar el usuario!"}
-        errorText={"Intenta nuevamente habilitar el usuario y si el problema persiste comunicate ccon soporte"}
-        onClose={() => setInnerModal(null)}
-        />
-      )}
     </section>
   );
 }
