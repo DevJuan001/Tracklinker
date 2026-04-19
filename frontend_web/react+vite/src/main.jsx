351c36f0e6b7cdd183ed6115bda465e5fa7cf888
DevJuan001 <juanesyt7@gmail.com>
2026-04-19 16:15:23 -0500

    feat: integrate @tanstack/react-query into main application for enhanced data management

--- frontend_web/react+vite/src/main.jsx
+++ frontend_web/react+vite/src/main.jsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import "./globals/styles/global.css";
 import App from "./App.jsx";
+import "./globals/styles/global.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
