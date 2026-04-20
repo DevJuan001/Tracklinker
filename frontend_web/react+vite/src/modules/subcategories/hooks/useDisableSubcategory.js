ee173fb1db3a60423a115975ab52f60d18498d90
DevJuan001 <juanesyt7@gmail.com>
2026-04-19 20:27:28 -0500

    refactor: simplify useDisableSubcategory by removing form state and integrating subcategory_id for improved service interaction and modal closure

--- frontend_web/react+vite/src/modules/subcategories/hooks/useDisableSubcategory.js
+++ frontend_web/react+vite/src/modules/subcategories/hooks/useDisableSubcategory.js
@@ -1,38 +1,26 @@
 import { useState } from "react";
 import { disableSubcategoryService } from "../services/disableSubcategoryService";
 
-export function useDisableSubcategory(formData) {
-  const [form, setForm] = useState(formData);
-  const [data, setData] = useState([]);
+export function useDisableSubcategory(subcategory_id) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  function handleChange(e) {
-    setForm((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-  }
-
-  // Función que pasa los parametros al service y valida la respuesta
-  async function handleSubmit(e, setInnerModal) {
+  async function handleSubmit(e, onClose) {
     e.preventDefault();
 
     setLoading(true);
 
     try {
-      const response = await disableSubcategoryService(form);
-      setData(response);
-      if (response.success) {
-        setInnerModal("success");
+      const response = await disableSubcategoryService(subcategory_id);
+      if (response.success === true) {
+        onClose();
       }
     } catch (error) {
-      setInnerModal("error");
       setError(error);
     } finally {
       setLoading(false);
     }
   }
 
-  return { form, data, loading, error, handleSubmit, handleChange };
+  return { loading, error, handleSubmit };
 }
