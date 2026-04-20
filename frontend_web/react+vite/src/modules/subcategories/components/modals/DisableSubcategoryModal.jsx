1c492d915f1465c4c5b6ad6742b8d03448f31f55
DevJuan001 <juanesyt7@gmail.com>
2026-04-19 20:22:50 -0500

    refactor: simplify DisableSubcategoryModal by removing inner modal state and integrating onClose for better user experience

--- frontend_web/react+vite/src/modules/subcategories/components/modals/DisableSubcategoryModal.jsx
+++ frontend_web/react+vite/src/modules/subcategories/components/modals/DisableSubcategoryModal.jsx
@@ -1,15 +1,10 @@
 // Hooks
-import { useState } from "react";
 import { useDisableSubcategory } from "../../hooks/useDisableSubcategory";
 // Componentes
 import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
-// Modales
-import ErrorModal from "../../../../globals/components/modals/ErrorModal";
-import SuccessModal from "../../../../globals/components/modals/SuccessModal";
 
 export default function DisableSubcategoryModal({ subcategory, onClose }) {
-  const [innerModal, setInnerModal] = useState(null);
   const { handleSubmit, loading } = useDisableSubcategory(
     subcategory.subcategory_id,
   );
@@ -25,34 +20,9 @@ export default function DisableSubcategoryModal({ subcategory, onClose }) {
         confirmBgColor="red-600"
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={(e) => handleSubmit(e, setInnerModal)}
+        confirmButtonOnClick={(e) => handleSubmit(e, onClose)}
         cancelButtonOnClick={onClose}
       />
-
-      {/* Modales Internas */}
-      {innerModal === "success" && (
-        <SuccessModal
-          isOpen={true}
-          confirmTitle={"Subcategoria deshabilitada con éxito!"}
-          confirmText={
-            "Se ha deshabilitado correctamente la subcategoria, toca el botón de volver a la pagina de subcategorias"
-          }
-          confirmButtonText={"Volver a la pagina"}
-          onClose={() => {
-            setInnerModal(null);
-            onClose();
-          }}
-        />
-      )}
-      {innerModal === "error" && (
-        <ErrorModal
-          isOpen={true}
-          errorTitle="¡No se pudo deshabilitar la subcategoria!"
-          errorText="No pudimos completar tu petición, por favor vuelve a intentarlo"
-          confirmButtonText="Volver a intentarlo"
-          onClose={() => setInnerModal(null)}
-        />
-      )}
     </section>
   );
 }
