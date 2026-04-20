e64411e9c36c1ae0a049b44da658aa41e4e13630
DevJuan001 <juanesyt7@gmail.com>
2026-04-19 22:06:53 -0500

    refactor: enhance SearchBar styling for improved dark mode support and input responsiveness

--- frontend_web/react+vite/src/globals/components/ui/SearchBar.jsx
+++ frontend_web/react+vite/src/globals/components/ui/SearchBar.jsx
@@ -2,16 +2,16 @@ import { headerIcons } from "../../../assets/icons/headerIcons";
 
 export default function SearchBar({ value, onChange }) {
   return (
-    <div className="flex px-2 w-[40%] border border-gray-300 rounded-3xl dark:border-[#37415173]">
+    <div className="flex px-2 w-[40%] border border-gray-300 rounded-3xl dark:bg-[#0f0f11] dark:border-[#34343a77]">
       <input
         id="search-text"
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder="Buscar"
-        className="w-[95%] py-2.5 px-5 rounded-xl outline-none
-            placeholder:text-[15px] placeholder:text-gray-500
-            dark:bg-black dark:placeholder-text-gray-700 dark:text-white"
+        className="w-full py-2.5 px-5 rounded-xl outline-none
+        placeholder:text-[15px] placeholder:text-gray-500
+        dark:bg-[#0f0f11] dark:placeholder:text-white dark:text-white"
       />
       <button>
         <img
