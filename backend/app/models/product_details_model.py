fe19dba0a03d3bd4031115c576c26970b197a54c
DevJuan001 <juanesyt7@gmail.com>
2026-04-23 13:22:49 -0500

    refactor: simplify ProductDetails model by consolidating attributes

--- backend/app/models/product_details_model.py
+++ backend/app/models/product_details_model.py
@@ -4,9 +4,7 @@ from typing import Optional
 
 class ProductDetails(BaseModel):
     product_details_id: Optional[int] = None
-    product_brand_id: int
-    product_detail_model: str
-    product_detail_description: str
+    model: int
 
 
 class UpdateProductDetails(BaseModel):
