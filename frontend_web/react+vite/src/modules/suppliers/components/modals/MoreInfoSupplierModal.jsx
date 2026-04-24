7299e051ddafcf32b1bf5691c26e8b1b08c27c76
DevJuan001 <juanesyt7@gmail.com>
2026-04-24 15:35:02 -0500

    refactor: enhance MoreInfoSupplierModal by integrating useInnerModal for improved modal functionality and updating supplier data structure

--- frontend_web/react+vite/src/modules/suppliers/components/modals/MoreInfoSupplierModal.jsx
+++ frontend_web/react+vite/src/modules/suppliers/components/modals/MoreInfoSupplierModal.jsx
@@ -1,21 +1,50 @@
-export default function MoreInfoSupplierModal({ supplier }) {
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
+import AddInnerModal from "../../../../globals/components/modals/AddInnerModal";
+import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+import EditSupplierInfoModal from "./EditSupplierInfoModal";
+
+export default function MoreInfoSupplierModal({ supplier, onClose }) {
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+
   return (
-    <section className="flex flex-col justify-center dark:text-white">
-      <p>
-        <strong>Nombre:</strong> {supplier.supplier_name}
-      </p>
-      <p>
-        <strong>Ciudad:</strong> {supplier.supplier_city}
-      </p>
-      <p>
-        <strong>Teléfono:</strong> {supplier.supplier_phone}
-      </p>
-      <p>
-        <strong>Dirección:</strong> {supplier.supplier_address}
-      </p>
-      <p>
-        <strong>Fecha De Creación:</strong> {supplier.supplier_date}
-      </p>
+    <section className="flex flex-col items-center dark:text-white">
+      <div className="self-start">
+        <p>
+          <strong>Nombre:</strong> {supplier.name}
+        </p>
+        <p>
+          <strong>Ciudad:</strong> {supplier.city}
+        </p>
+        <p>
+          <strong>Teléfono:</strong> {supplier.phone}
+        </p>
+        <p>
+          <strong>Dirección:</strong> {supplier.address}
+        </p>
+        <p>
+          <strong>Fecha De Creación:</strong> {supplier.date}
+        </p>
+      </div>
+
+      <ConfirmCancelButtons
+        confirmText="Editar"
+        confirmButtonOnClick={(e) => openInnerModal("edit", e)}
+        cancelButtonOnClick={onClose}
+      />
+
+      {innerType === "edit" && (
+        <AddInnerModal
+          isOpen={true}
+          onClose={() => openInnerModal(null)}
+          title={"Editar Usuario"}
+          triggerRef={innerTrigger}
+        >
+          <EditSupplierInfoModal
+            supplier={supplier}
+            onClose={() => openInnerModal(null)}
+          />
+        </AddInnerModal>
+      )}
     </section>
   );
 }
