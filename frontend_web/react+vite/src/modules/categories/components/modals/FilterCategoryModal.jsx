557d07270129e70c13f6a2cb684c34f879d9e0c6
DevJuan001 <juanesyt7@gmail.com>
2026-04-27 13:34:16 -0500

    feat: refactor FilterCategoryModal to streamline filter application and enhance modal structure

--- frontend_web/react+vite/src/modules/categories/components/modals/FilterCategoryModal.jsx
+++ frontend_web/react+vite/src/modules/categories/components/modals/FilterCategoryModal.jsx
@@ -2,20 +2,14 @@ import { useFilterCategories } from "../../hooks/useFilterCategories";
 import SelectMenu from "../../../../globals/components/modals/SelectMenu";
 import FilterModal from "../../../../globals/components/modals/FilterModal";
 
-export default function FilterCategoryModal({ refetch, onClose }) {
-  const { form, handleChange, handleApply } = useFilterCategories(
-    {
-      start_date: "",
-      end_date: "",
-      name_order: "",
-    },
-    refetch,
-  );
+export default function FilterCategoryModal({ setFilters, onClose }) {
+  const { form, handleChange } = useFilterCategories();
+
   return (
     <FilterModal
       applyButtonOnClick={() => {
+        setFilters({ ...form });
         onClose();
-        handleApply();
       }}
       orderByStartDateValue={form.start_date}
       orderByStartDateOnChange={handleChange}
@@ -23,27 +17,29 @@ export default function FilterCategoryModal({ refetch, onClose }) {
       orderByFinishDateOnChange={handleChange}
       onClose={onClose}
     >
-      <SelectMenu
-        spanText={"Nombres"}
-        name={"name_order"}
-        onChange={handleChange}
-        value={form.name_order}
-        options={[
-          { value: "asc", label: "a - Z" },
-          { value: "desc", label: "Z - a" },
-        ]}
-      />
+      <div className="flex flex-col gap-2">
+        <SelectMenu
+          spanText={"Nombres"}
+          name={"name_order"}
+          onChange={handleChange}
+          value={form.name_order}
+          options={[
+            { value: "asc", label: "a - Z" },
+            { value: "desc", label: "Z - a" },
+          ]}
+        />
 
-      <SelectMenu
-        spanText={"Estado"}
-        value={form.status}
-        name={"status"}
-        onChange={handleChange}
-        options={[
-          { value: 1, label: "Deshabilitada" },
-          { value: 2, label: "Activa" },
-        ]}
-      />
+        <SelectMenu
+          spanText={"Estado"}
+          value={form.status}
+          name={"status"}
+          onChange={handleChange}
+          options={[
+            { value: 1, label: "Deshabilitada" },
+            { value: 2, label: "Activa" },
+          ]}
+        />
+      </div>
     </FilterModal>
   );
 }
