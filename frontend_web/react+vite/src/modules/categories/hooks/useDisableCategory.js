e52e8d26111c0a9d17bf68bb6084a2b50c25d434
DevJuan001 <juanesyt7@gmail.com>
2026-04-27 13:29:02 -0500

    feat: update useDisableCategory hook to improve error handling and integrate query invalidation

--- frontend_web/react+vite/src/modules/categories/hooks/useDisableCategory.js
+++ frontend_web/react+vite/src/modules/categories/hooks/useDisableCategory.js
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import { disableCategoryService } from "../services/disableCategoryService";
+import { useQueryClient } from "@tanstack/react-query";
 
 export function useDisableCategory(id) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const queryClient = useQueryClient();
 
   // Función que envía el ID al service y maneja la respuesta
-  async function handleDisable(setInnerModal) {
+  async function handleDisable(onClose) {
     setLoading(true);
 
     try {
       const response = await disableCategoryService(id);
-      if (response.success) {
-        setInnerModal("success");
+      if (response.success === true) {
+        queryClient.invalidateQueries({ queryKey: ["categories"] });
+        onClose();
       }
       setData(response);
     } catch (error) {
-      setInnerModal("error");
       setError(error);
     } finally {
       setLoading(false);
