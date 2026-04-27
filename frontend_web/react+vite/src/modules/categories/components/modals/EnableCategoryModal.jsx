acddfa06748ef5633a84a0a9af63d1e36b490d99
DevJuan001 <juanesyt7@gmail.com>
2026-04-27 13:33:46 -0500

    feat: refactor EnableCategoryModal to standardize category property names and simplify modal interactions

--- frontend_web/react+vite/src/modules/categories/components/modals/EnableCategoryModal.jsx
+++ frontend_web/react+vite/src/modules/categories/components/modals/EnableCategoryModal.jsx
@@ -1,21 +1,17 @@
 // Hooks
-import { useState } from "react";
 import { useEnableCategory } from "../../hooks/useEnableCategory";
 // Componentes
-import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
-// Modales
-import ErrorModal from "../../../../globals/components/modals/ErrorModal";
-import SuccessModal from "../../../../globals/components/modals/SuccessModal";
 import Loader from "../../../../globals/components/ui/Loader";
+import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
 
 export default function EnableCategoryModal({ category, onClose }) {
-  const [innerModal, setInnerModal] = useState(null);
-  const { loading, handleEnable } = useEnableCategory(category.category_id);
+  const { loading, handleEnable } = useEnableCategory(category.id);
+
   return (
     <section className="flex flex-col justify-center items-center dark:text-white">
       <p>
         ¿Seguro que deseas habilitar la categoría{" "}
-        <span className="font-medium">{category.category_name}</span>?
+        <span className="font-medium">{category.name}</span>?
       </p>
 
       {/* Botones */}
@@ -23,35 +19,9 @@ export default function EnableCategoryModal({ category, onClose }) {
         confirmText={loading ? <Loader /> : "Habilitar"}
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={() => handleEnable(setInnerModal)}
+        confirmButtonOnClick={() => handleEnable(onClose)}
         cancelButtonOnClick={onClose}
       />
-
-      {/* Modales Internas */}
-      {innerModal === "success" && (
-        <SuccessModal
-          isOpen={true}
-          confirmTitle={"Categoría habilitada con éxito!"}
-          confirmText={
-            "La categoría fue habilitada correctamente. Toca el botón para volver."
-          }
-          confirmButtonText={"Volver a la página"}
-          onClose={() => {
-            setInnerModal(null);
-            onClose();
-          }}
-        />
-      )}
-
-      {innerModal === "error" && (
-        <ErrorModal
-          isOpen={true}
-          errorTitle="¡No se pudo completar está acción!"
-          errorText="Vuelve a intentar esta acción, si el error sigue comunicate con servicio al cliente"
-          confirmButtonText="Volver a intentarlo"
-          onClose={() => setInnerModal(null)}
-        />
-      )}
     </section>
   );
 }
