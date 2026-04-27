3a70a42b0fccfc1f59df0eaca0d6ba76dcfd71c3
DevJuan001 <juanesyt7@gmail.com>
2026-04-27 14:00:40 -0500

    feat: update useSuppliers hook to support filtering by integrating filters state

--- frontend_web/react+vite/src/modules/suppliers/hooks/useSuppliers.js
+++ frontend_web/react+vite/src/modules/suppliers/hooks/useSuppliers.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { getSuppliersService } from "../services/getSuppliersService";
 import { useQuery } from "@tanstack/react-query";
 
 export function useSuppliers() {
+  const [filters, setFilters] = useState();
+
   const suppliers = useQuery({
     queryKey: ["suppliers"],
-    queryFn: getSuppliersService,
+    queryFn: () => getSuppliersService(filters),
     staleTime: 1000 * 60 * 10,
   });
 
@@ -12,5 +15,6 @@ export function useSuppliers() {
     suppliers: suppliers.data || [],
     loading: suppliers.isLoading,
     error: suppliers.error,
+    setFilters,
   };
 }
